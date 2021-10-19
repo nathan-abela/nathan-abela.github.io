a09bc2c0bd896e94ba9f02fc1fe3a265cab2237d
Nathan Abela <nathanabela7@gmail.com>
2021-10-20 01:48:58 +0200

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Title, Text } from '@components';
+
+const NotFound = (): JSX.Element => (
+    <Container alignItems="center">
+        <Title>404 - Page Not found</Title>
+        <Text>The page does not exist!</Text>
+    </Container>
+);
+
+export default NotFound;
