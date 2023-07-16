d7548c9f76a2727264ea17b4ea99039784ce5161
Nathan Abela <nathanabela7@gmail.com>
2023-07-16 16:51:53 +0200

    chore(config): add `basePath` configuration

--- next.config.js
+++ next.config.js
@@ -3,15 +3,18 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = './';
+let basePath = '';
 
 if (isGithubActions) {
 	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
 	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
 }
 
 module.exports = {
 	assetPrefix: assetPrefix,
+	basePath: basePath,
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg?$/,
