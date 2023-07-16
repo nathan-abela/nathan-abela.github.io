d8f00231b890eeb8da16f02b660c91f29459669e
Nathan Abela <nathanabela7@gmail.com>
2023-07-16 17:02:12 +0200

    chore(config): update `basePath` and `assetPath`

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 // next.config.js
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = './';
-let basePath = '';
-
-if (isGithubActions) {
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-	assetPrefix = `/${repo}/`;
-	basePath = `/${repo}`;
-}
-
 module.exports = {
-	assetPrefix: assetPrefix,
-	basePath: basePath,
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/nathan-abela.github.io' : '',
+	basePath: process.env.NODE_ENV === 'production' ? '/nathan-abela.github.io' : '',
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg?$/,
