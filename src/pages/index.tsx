0db2eafe7a6f21ebcc059f1610773ca8b4f36279
Nathan Abela <nathanabela7@gmail.com>
2023-08-15 23:48:36 +0200

    refactor(home page): update description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,7 @@ function Home() {
 				</div>
 				<p style={{ lineHeight: 1.8 }}>
 					I am a passionate software engineer from Malta, currently pursuing a Bachelor of Science in Software
-					Development. I specialise in frontend development using Angular, StencilJS, and NextJS, where I have
-					demonstrated proficiency in developing mobile apps, building web applications, and designing RESTful APIs.
-					With a strong focus on effective communication, problem-solving, and collaborative teamwork. Explore my{' '}
+					Development. I specialise in frontend development using Angular, StencilJS, and NextJS. My{' '}
 					{/* eslint-disable-next-line prettier/prettier */}
 					<a
 						href="/portfolio"
@@ -44,7 +42,8 @@ function Home() {
 					>
 						portfolio
 					</a>{' '}
-					for a glimpse of my projects.
+					highlights a range of successful projects, both for academic purposes and personal initiatives. I possess a
+					strong focus on effective communication, problem-solving, and collaborative teamwork.
 				</p>
 
 				<Button href="/portfolio">Portfolio</Button>
