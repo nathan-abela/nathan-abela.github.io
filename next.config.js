c101ecfcfd975310cc224daf43b29730faef6534
Nathan Abela <nathanabela7@gmail.com>
2023-08-26 15:56:29 +0200

    chore(config): add image loader configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
 module.exports = {
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+
+	images: {
+		loader: 'akamai',
+		path: '',
+	},
+
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg?$/,
