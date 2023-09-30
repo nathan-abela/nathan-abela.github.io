468d08df49f9264548b85bd4de7c1dbe8d790cb8
Nathan Abela <nathanabela7@gmail.com>
2023-09-30 21:54:13 +0200

    test(button): add unit tests

---
+++ src/components/atoms/Button/Button.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+
+import { Button } from '../Button';
+
+describe('Button', () => {
+	it('renders button with link correctly', () => {
+		const { container } = render(
+			<Button href="/url" target="_blank">
+				Button text
+			</Button>
+		);
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders button correctly', () => {
+		const { container } = render(
+			// eslint-disable-next-line prettier/prettier
+			<Button onClick={jest.fn}>Button text</Button>
+		);
+		expect(container).toMatchSnapshot();
+	});
+});
