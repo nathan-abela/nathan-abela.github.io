b3d88354dc7ba67a3f539319c37cc48545673fbc
Nathan Abela <nathanabela7@gmail.com>
2023-09-30 21:50:33 +0200

    feat(jest): add jest setup for unit testing

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+process.env.TZ = 'GMT';
+
+module.exports = {
+	roots: ['<rootDir>'],
+	testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx', '**/*.test.ts', '**/*.test.tsx'],
+	setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
+	testPathIgnorePatterns: ['<rootDir>/build/', '/dist/', '<rootDir>/node_modules/'],
+	moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+	collectCoverageFrom: [
+		'**/src/**/**/*.{js,jsx,ts,tsx}',
+		'src/**/*.{js,jsx,ts,tsx}',
+		'!src/redux/**/index.ts',
+		'!**/src/components/**/*.stories.+(js|ts|tsx)',
+		'!**/__tests__/**',
+		'!**/node_modules/**',
+		'!**/.next/**',
+		'!**/build/**',
+	],
+	reporters: [
+		'default',
+		[
+			'jest-html-reporter',
+			{
+				pageTitle: 'Unit Tests Report',
+				outputPath: './test/tests-report/unit-test-report.html',
+				includeFailureMsg: true,
+				includeConsoleLog: true,
+			},
+		],
+	],
+	coverageThreshold: {
+		global: {
+			statements: 80,
+			branches: 75,
+			functions: 80,
+			lines: 80,
+		},
+	},
+	projects: ['./test/jest.lint.js', './test/jest.unittest.js'],
+};
