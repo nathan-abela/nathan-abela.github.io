35a598dd765de5e3f333c0b950e2480c5338dcd2
Nathan Abela <nathanabela7@gmail.com>
2023-10-15 00:55:15 +0200

    test(portfolio card): add unit tests

---
+++ src/components/atoms/PortfolioCard/PortfolioCard.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import { PortfolioCard } from './PortfolioCard';
+
+describe('Portfolio Card', () => {
+	it('renders portfolio card correctly', () => {
+		const { container } = render(
+			<PortfolioCard
+				item={{
+					title: 'Sample Project',
+					slug: 'sample-project',
+					link: 'https://example.com',
+					type: 'Web Development',
+					technologies: ['HTML', 'CSS', 'JavaScript'],
+					content: 'This is a sample project description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+				}}
+			></PortfolioCard>
+		);
+		expect(container).toMatchSnapshot();
+	});
+});
