9ecedcb987cd6e66c0a5499b21dd6703e6e1058a
Nathan Abela <nathanabela7@gmail.com>
2023-10-17 08:33:50 +0200

    test(theme toggle): add unit tests

---
+++ src/components/atoms/ThemeToggle/ThemeToggle.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+
+import { ThemeToggle } from './ThemeToggle';
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+	useSelector: jest.fn(),
+	useDispatch: () => mockDispatch,
+}));
+
+describe('ThemeToggle', () => {
+	it('renders theme toggle correctly', () => {
+		const { container } = render(
+			// eslint-disable-next-line prettier/prettier
+			<ThemeToggle />
+		);
+		expect(container).toMatchSnapshot();
+	});
+});
